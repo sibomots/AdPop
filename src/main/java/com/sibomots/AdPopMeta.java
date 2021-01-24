81526d81162376a659662030fe5aa96883a16d20
sibomots <sibomots@mondolab.net>
2021-01-24 04:22:11 +0000

    sample code

---
+++ src/main/java/com/sibomots/AdPopMeta.java
@@ -0,0 +1,45 @@
+package com.sibomots.adpop;
+
+// import java.io.File;
+// import java.io.IOException;
+// import java.net.URL;
+// import java.nio.file.Files;
+// import java.nio.file.Path;
+// import java.util.Random;
+// import java.util.concurrent.TimeUnit;
+// 
+// import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+// import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+// import ninja.leaping.configurate.loader.ConfigurationLoader;
+// 
+// import org.slf4j.Logger;
+// import org.spongepowered.api.Game;
+// import org.spongepowered.api.Sponge;
+// import org.spongepowered.api.command.CommandException;
+// import org.spongepowered.api.command.CommandResult;
+// import org.spongepowered.api.command.args.GenericArguments;
+// import org.spongepowered.api.command.spec.CommandSpec;
+// import org.spongepowered.api.config.ConfigDir;
+// import org.spongepowered.api.config.DefaultConfig;
+// import org.spongepowered.api.entity.living.player.Player;
+// import org.spongepowered.api.event.Listener;
+// import org.spongepowered.api.event.game.GameReloadEvent;
+// import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+// import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
+// import org.spongepowered.api.plugin.Plugin;
+// import org.spongepowered.api.scheduler.Task;
+// import org.spongepowered.api.text.Text;
+// import org.spongepowered.api.text.Text.Builder;
+// import org.spongepowered.api.text.action.TextActions;
+// import org.spongepowered.api.text.serializer.TextSerializers;
+// 
+// import com.google.inject.Inject;
+
+public class AdPopMeta
+{
+    public static final String ID      = "adpopmod";
+    public static final String NAME    = "AdPopMod";
+    public static final String VERSION = "1.0";
+    public static final String AUTHOR  =  "sibomots"; 
+    public static final String ABOUT   = "Messages for the people";
+}
